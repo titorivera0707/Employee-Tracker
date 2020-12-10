0fcedafb8637d70088e3866c3ac706dea60b5a67
Tito Rivera <titorivera0707@gmail.com>
2020-12-09 18:51:08 -0800

    Updating some of the functions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const { resolveSoa } = require("dns");
+const { resolveSoa, ADDRGETNETWORKPARAMS } = require("dns");
 const inquirer = require("inquirer");
 var mysql = require("mysql");
+const { allowedNodeEnvironmentFlags } = require("process");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -53,6 +54,69 @@ function initial() {
     })
 }
 
+function add() {
+    inquirer.prompt({
+        type: "list",
+        name: "add",
+        message: "What would you like to add?",
+        choices: ["Departments", "Roles", "Employees"]
+    }).then((response) => {
+        if(response.add === "Departments"){
+            addDep()
+        }
+        if(response.add === "Roles") {
+            addRole()
+        }
+        if(response.add === "Employees") {
+            AddEmp()
+        }
+    })
+    function addDep() {
+        inquirer.prompt({
+            type: "input",
+            name: "Dep",
+            message: "What department would you like to add?"
+        }).then((response) => {
+            connection.query("INSERT INTO department (name) VALUES (?)", [response.Dep], function(err, data) {
+                if (err) throw err;
+                console.log("Department successfully added!")
+            })
+        })
+    }
+    function addRole() {
+        connection.query("SELECT * FROM department", function(err, res) {
+            var departments = res
+            console.log(res)
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "addRole",
+                message: "What would you like to name your new role?"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What will the salary be?"
+            },
+            {
+            type: "list",
+            name: "list",
+            message: "What department would you like to choose?",
+            choices: departments
+            }
+        ]).then((response) => {
+            console.log(response.list)
+            connection.query("SELECT id FROM department WHERE ?", {name: response.list}, function(err, res) {
+                console.log(res)
+                connection.query("INSERT INTO role SET ?", [response.addRole, response.salary, response.list], function(err, res) {
+                    console.log("Role Successfully added!")
+                })
+            })
+        })
+    })
+    }
+}
+
 function viewer() {
     inquirer.prompt({
         type: "list",
@@ -75,22 +139,19 @@ function viewer() {
 function roles() {
     connection.query("SELECT * FROM role", function(err, res) {
         if (err) throw err;
-        
+        console.log(res)
     })
 }
 
 function departments() {
-    connection.query("SELECT * FROM departments", function(err, res) {
+    connection.query("SELECT * FROM department", function(err, res) {
         console.log(res)
-        console.log(err)
-        console.log("HELLO")
+        res.json()
     })
 }
 
 function employees() {
-    connection.query("SELECT * FROM employees", function(err, res) {
+    connection.query("SELECT * FROM employee", function(err, res) {
         console.log(res)
-        console.log(err)
-        console.log("HELLO")
     })
 }
